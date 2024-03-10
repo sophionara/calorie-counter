5ad60194666bee884c42e4ded97e89893862df21
sophionara <chaphidzes@gmail.com>
2024-03-10 13:22:09 +0400

    Add explanatory comments

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+//CREATE REFERENCES TO MANIPULATE CORRESPONDING HTML ELEMENTS OR RETRIEVE THEIR PROPERTIES
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+// USED TO TRACK WHETHER AN ERROR HAS OCCURRED IN THE PROGRAM
 let isError = false;
 
+// REMOVES +/-/WHITESPACE FROM STRING
 function cleanInputString(str) {
+  //REGEX = REGULAR EXPRESSION OBJECT 
   const regex = /[+-\s]/g;
+  //\s - WHITESPACE, \g - GLOBAL FLAG
   return str.replace(regex, '');
 }
 
 function isInvalidInput(str) {
+  //i-MAKES MATCHING CASE-INSENSITIVE
   const regex = /\d+e\d+/i;
+  //RETURN ARRAY
   return str.match(regex);
 }
 
+//HANDLE ADDING NEW ENTRIES TO THE CALORIE COUNTER BASED ON USER INPUT
 function addEntry() {
+  //HOOKING TO SPECIFIC INPUT SECTION, BY CONNECTING ID AND CLASS CONTAINER
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  //+1 BECAUSE WE WANT TO GET "Entry 1 Name", WITHOUT THAT WE WOULD GET "Entry 0 Name"
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
@@ -29,10 +39,12 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
+  //INSERTS THE HTML CONTENT REPRESENTED BY 'HTMLString' INTO THE DOM, SPECIFICALLY JUST BEFORE THE END OF 'targetInputContainer'
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
 function calculateCalories(e) {
+  //PREVENT THE DEFAULT ACTION ASSOCIATED WITH THE EVENT, OVERRIDING THE DEFAULT BEHAVIOR
   e.preventDefault();
   isError = false;
 
@@ -67,13 +79,15 @@ function calculateCalories(e) {
   output.classList.remove('hide');
 }
 
+//CALCULATE THE TOTAL CALORIES FROM A LIST OF INPUT VALUES
 function getCaloriesFromInputs(list) {
   let calories = 0;
-
+  //FOR .. OF LOOP
   for (const item of list) {
+    //CLEANING THE VALUE AND STORING IN VARIABLE currVal
     const currVal = cleanInputString(item.value);
     const invalidInputMatch = isInvalidInput(currVal);
-
+    //IF invalidInputMatch IS TRUTHY
     if (invalidInputMatch) {
       alert(`Invalid Input: ${invalidInputMatch[0]}`);
       isError = true;
